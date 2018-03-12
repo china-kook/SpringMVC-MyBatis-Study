a7edb4d4d79ea149bac291125c23c6026b1f2399
ikook <china.ikook@gmail.com>
2018-03-12 23:06:27 +0800

    模糊查询和普通查询

--- MyBatisFirstDemo/src/com/ikook/mybatis/po/User.java
+++ MyBatisFirstDemo/src/com/ikook/mybatis/po/User.java
@@ -1,7 +1,7 @@
 package com.ikook.mybatis.po;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.sql.Date;
 
 public class User implements Serializable {
 
--- MyBatisFirstDemo/src/com/ikook/mybatis/test/MyBatisTest.java
+++ MyBatisFirstDemo/src/com/ikook/mybatis/test/MyBatisTest.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class MyBatisTest {
 
@@ -20,16 +20,24 @@ public class MyBatisTest {
 
         // sqlSession.selectOne 最终结果是映射文件中所匹配的 resultType 类型
         User user = sqlSession.selectOne("test.findUserById", 1);
-        System.out.println("姓名:"+user.getUsername());
-        System.out.println("性别:"+user.getGender());
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-        System.out.println("生日:"+simpleDateFormat.format(user.getBirthday()));
-        System.out.println("所在地:"+user.getProvince()+user.getCity());
+        System.out.println(user);
 
         sqlSession.close();
 
+    }
+
+    @Test
+    public void TestFuzzySearch() throws IOException {
+        SqlSession sqlSession = dataConnention.getSqlSession();
+
+        List<User> userList = sqlSession.selectList("test.findUserByUsername", "丽");
+
+        for (int i = 0; i < userList.size(); i++) {
+            User u = userList.get(i);
+            System.out.println(u);
+        }
+
+        sqlSession.close();
     }
 
 }

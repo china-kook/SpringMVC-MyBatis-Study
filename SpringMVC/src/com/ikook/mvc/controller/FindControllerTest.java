e7e3ae5913570b77dc9a11c6acf9fe8460f4a6e8
ikook <china.ikook@gmail.com>
2018-03-15 02:51:10 +0800

    数值映射与参数绑定

--- SpringMVC/src/com/ikook/mvc/controller/FindControllerTest.java
+++ SpringMVC/src/com/ikook/mvc/controller/FindControllerTest.java
@@ -1,6 +1,8 @@
 package com.ikook.mvc.controller;
 
 import com.ikook.mvc.model.Fruits;
+import com.ikook.mvc.model.ListQryModel;
+import com.ikook.mvc.model.MapQryModel;
 import com.ikook.mvc.service.FruitsService;
 import com.ikook.mvc.service.FruitsServiceImpl;
 
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/query")
@@ -41,6 +44,30 @@ public class FindControllerTest {
 
         return "/fruits/findFruits";
 
+    }
+
+
+    @RequestMapping("fruitsArrayTest")
+    public void FruitsArray(Model model,int[] fids){
+        for (int i = 0; i < fids.length; i++) {
+            System.out.println("fids["+i+"]="+fids[i]);
+        }
+    }
+
+    @RequestMapping("fruitsListTest")
+    public void FruitsList(Model model,ListQryModel listQryModel){
+        List<Fruits> fruitList=listQryModel.getFruitList();
+        for (int i = 0; i < fruitList.size(); i++) {
+            System.out.println("fruitList["+i+"].name="+fruitList.get(i).getName());
+        }
+    }
+
+    @RequestMapping("fruitsMapTest")
+    public void FruitsMap(Model model,MapQryModel MapQryModel){
+        Map<String,Object> fruitMap=MapQryModel.getFruitMap();
+        for(String key:fruitMap.keySet()){
+            System.out.println("fruitMap["+key+"]="+fruitMap.get(key));
+        }
     }
 
 
---
+++ SpringMVC/src/com/ikook/mvc/model/ListQryModel.java
@@ -0,0 +1,16 @@
+package com.ikook.mvc.model;
+
+import java.util.List;
+
+public class ListQryModel {
+    private List<Fruits> fruitList;
+
+    public List<Fruits> getFruitList() {
+        return fruitList;
+    }
+
+    public void setFruitList(List<Fruits> fruitList) {
+        this.fruitList = fruitList;
+    }
+
+}

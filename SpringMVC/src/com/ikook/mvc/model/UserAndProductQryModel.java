e94e07630be702dac74cf6338375ad2aecb6d23c
ikook <china.ikook@gmail.com>
2018-03-15 02:29:14 +0800

    包装类型参数绑定

---
+++ SpringMVC/src/com/ikook/mvc/model/UserAndProductQryModel.java
@@ -0,0 +1,23 @@
+package com.ikook.mvc.model;
+
+public class UserAndProductQryModel {
+    private User user;
+    private Fruits userFruits;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Fruits getUserFruits() {
+        return userFruits;
+    }
+
+    public void setUserFruits(Fruits userFruits) {
+        this.userFruits = userFruits;
+    }
+
+}

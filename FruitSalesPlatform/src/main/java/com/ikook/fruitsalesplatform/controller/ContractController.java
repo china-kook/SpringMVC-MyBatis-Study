7db6cdbb0aa9e4924a2fb0b0125864f1a4cc3c26
ikook <china.ikook@gmail.com>
2018-03-18 21:52:30 +0800

    购销合同管理模块：关联商品完成

--- FruitSalesPlatform/src/main/java/com/ikook/fruitsalesplatform/controller/ContractController.java
+++ FruitSalesPlatform/src/main/java/com/ikook/fruitsalesplatform/controller/ContractController.java
@@ -1,8 +1,11 @@
 package com.ikook.fruitsalesplatform.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ikook.fruitsalesplatform.entity.Commodities;
 import com.ikook.fruitsalesplatform.entity.ContractVo;
 import com.ikook.fruitsalesplatform.entity.Retailer;
+import com.ikook.fruitsalesplatform.service.AccessoryService;
+import com.ikook.fruitsalesplatform.service.CommoditiesService;
 import com.ikook.fruitsalesplatform.service.ContractService;
 import com.ikook.fruitsalesplatform.service.RetailerService;
 import com.mysql.jdbc.StringUtils;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +31,12 @@ public class ContractController {
 
     @Resource
     private RetailerService retailerService;
+
+    @Resource
+    private CommoditiesService commoditiesService;
+
+    @Resource
+    private AccessoryService accessoryService;
 
     //跳转至列表页面
     @RequestMapping("/contract/list.action")
@@ -56,6 +66,45 @@ public class ContractController {
 
         List<Retailer> retailerList = retailerService.find(param);
         return retailerList;
+    }
+
+    @RequestMapping("/contract/getAllCommodities.action")
+    public @ResponseBody
+    List<Commodities> getAllCommodities(@RequestBody String json) {
+
+        Map<String, Object> param = new HashMap<String, Object>();
+
+        if (!StringUtils.isNullOrEmpty(json)) {
+            String name = JSONObject.parseObject(json).getString("name");
+            if (!StringUtils.isNullOrEmpty(name)) {
+                param.put("name", "%" + name + "%");//商品名
+            }
+        }
+
+        List<Commodities> commoditiesList = commoditiesService.find(param);
+
+        return commoditiesList;
+    }
+
+    @RequestMapping("/contract/getCommoditiesAndAccessory.action")
+    public @ResponseBody
+    List<Map<String, Object>> getCommoditiesAndAccessory(String[] arrays) {
+
+        List<Map<String, Object>> cList = new ArrayList<>();
+
+        Map<String, Object> cMap;
+
+        for (int i = 0; i < arrays.length; i++) {
+            cMap = new HashMap<>();
+            String fruitId = arrays[i];
+            cMap.put("commodities", commoditiesService.get(fruitId));//获取货物信息
+            Map<String, String> param = new HashMap<>();
+            param.put("fruitId", fruitId);
+            cMap.put("accessory", accessoryService.find(param));//获取货物下的附属品信息
+            cList.add(cMap);//添加至List
+        }
+
+        return cList;
     }
 
 }
